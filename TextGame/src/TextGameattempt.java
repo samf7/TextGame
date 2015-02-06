876df89ccc230c0d11ac4c8d859cd8948639b89a
samf7 <samfoley11@gmail.com>
2015-02-05 19:33:25 -0500

    I WIN

---
+++ TextGame/src/TextGameattempt.java
@@ -0,0 +1,130 @@
+
+
+import java.util.Scanner;
+
+public class TextGameattempt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String choice;
+		Scanner user_in = new Scanner(System.in);
+ 
+		
+		while(true){
+				
+		System.out.println("You wake up on a school morning" +"\n\"\"Press 1 To sleep for 5 more minutes, Press 2 to get out of bed now") ;
+		choice = user_in.nextLine();
+		if(choice.equals("1")) {
+			System.out.println("You over sleep and dont make it to school... Game Over") ;
+			continue ;
+        }
+		if(choice.equals("2")) {
+			System.out.println("You get out of bed and soon it comes time to leave for school" +"\n\"\"You are preparing to leave for school.");
+		}
+		if(choice.equals("4")) {
+			System.out.println("FOUR IS THE GREATEST ALL HAIL CODETHULU") ;
+		}
+	
+		System.out.println("Press 1 to ask for a ride to school from your parents.  Press 2 to walk to school");
+		choice= user_in.nextLine();
+		if(choice.equals("1")){
+			System.out.println("You recieve a ride to school" +"\n\"\"You get to school at 7:15");
+		}
+		if(choice.equals("2")) {
+			System.out.println("You begin walking and slip and fall... Game Over") ;
+			continue ;
+		}
+		if(choice.equals("4")) {
+			System.out.println("FOUR IS THE GREATEST ALL HAIL CODETHULU") ;
+		}
+	
+		System.out.println("Press 1 to wait in your history class and finish your homework.  Press 2 to wait in the cafeteria and finish your homework") ;
+		choice=user_in.nextLine();
+		if(choice.equals("1")) {
+			System.out.println("You finish your homework and have time to talk to your friends" +"\n\"\"Your class begins and eventually ends, on your way to your next class the fire drill goes off");
+		}
+		if(choice.equals("2")) {
+			System.out.println("Somebody spills a drink on your homework... Game Over") ;
+			continue ;
+		}
+		if(choice.equals("4")) {
+			System.out.println("FOUR IS THE GREATEST ALL HAIL CODETHULU") ;
+		}
+	
+		System.out.println("Press 1 to follow the appropriate protocol of a firedrill Press 2 to sneak away and begin walking home");
+		choice=user_in.nextLine();
+		if(choice.equals("1")) {
+			System.out.println("You follow the rules and slowly become a boring person and fade into obscurity... Game Over") ;
+			continue ;
+		}
+		if(choice.equals("2")){
+			System.out.println("You begin walking home and a black van pulls up next to you and a man in a suit tells you to get in");
+		}
+		if(choice.equals("4")) {
+			System.out.println("FOUR IS THE GREATEST ALL HAIL CODETHULU") ;
+		}
+	
+		System.out.println("Press 1 to listen to the man and get in, Press 2 too disobey and keep walking");
+		choice=user_in.nextLine();
+		if(choice.equals("1")) {
+			System.out.println("The man in the van shows you his FBI badge and begins to brief you on a secret mission" +"\n\"\"'You have been chosen to travel back in time and assassinate Ronald Reagan, do you accept' The man asks");
+		}
+		if(choice.equals("2")) {
+			System.out.println("You continue walking home soon after a horde of robots come from the sky and destroys the earth... Game Over");
+			continue ;
+		}
+		if(choice.equals("4")) {
+			System.out.println("FOUR IS THE GREATEST ALL HAIL CODETHULU") ;
+		}
+	
+		System.out.println("Press 1 to accept, Press 2 to Decline");
+		choice=user_in.nextLine();
+		if(choice.equals("1")) {
+			System.out.println("The man signals the driver and they drive you to a secret facility located underneath a dunkin donuts."  +"\n\"\"You enter a time machine and are zapped back to 1949, you ask a random pedestrian 'where am I' the random person responds with 'Bel-Air LA'.  You need to get to where Ronald Reagan is currently");
+		}
+		if(choice.equals("2")){
+			System.out.println("The Man makes you leave the van and says 'you have doomed us all' soon after a horde of robots come from the sky and destroys the earth... Game Over");
+			continue ;
+		}
+		if(choice.equals("4")) {
+			System.out.println("FOUR IS THE GREATEST ALL HAIL CODETHULU") ;
+		}
+	
+		System.out.println("Press 1 to take a right down the road, Press 2 to travel straight down the road");
+		choice=user_in.nextLine();
+		if(choice.equals("1")) {
+			System.out.println("You find a expensive looking hotel and go inside to ask if anyone knew where the famous movie star Mr. Reagan is"  +"\n\"\"The woman at the desk tells you that he is infact staying inside of the hotel but refuses to tell you the room number.");
+		}
+		if(choice.equals("2")) {
+			System.out.println("You find a expensive looking hotel and go inside to ask if anyone knew where the famous movie star Mr. Reagan is"  +"\n\"\"The woman at the desk tells you that he is infact staying inside of the hotel but refuses to tell you the room number.");
+		}
+		if(choice.equals("4")) {
+			System.out.println("FOUR IS THE GREATEST ALL HAIL CODETHULU") ;
+		}
+	
+		System.out.println("Press 1 to look through the hotel and try to figure out what room Reagan is in, Press 2 to wait in the lobby for him to come out");
+		choice=user_in.nextLine();
+		if(choice.equals("1")) {
+			System.out.println("4444444444444444444444444444444444444FOURRRRRRRFOOOOOOOOOOOOOOOOOUUUUUUUUUUUUUUUUUUUURRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRFFFFFFFFFFFOOOOOOUUUUUUUUUUURRRRRRRRRRRRRRRRRRRRRRRFFFFFFFFFFFFFFFFOOOOOOOOOOOOOUUUUUUUUUUUUUUUUUUUUURRRRRRRRRRRRRRRRRR");
+		}
+		if(choice.equals("2")) {
+			System.out.println("4444444444444444444444444444444444444FOURRRRRRRFOOOOOOOOOOOOOOOOOUUUUUUUUUUUUUUUUUUUURRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRFFFFFFFFFFFOOOOOOUUUUUUUUUUURRRRRRRRRRRRRRRRRRRRRRRFFFFFFFFFFFFFFFFOOOOOOOOOOOOOUUUUUUUUUUUUUUUUUUUUURRRRRRRRRRRRRRRRRR");
+		}
+		if(choice.equals("3")) {
+			System.out.println("You have won the game for likely being a jerk and pressing 3 and not following the rules, congrats for not being a boring person") ;
+		}
+		if(choice.equals("4")) {
+			System.out.println("FOUR IS THE GREATEST ALL HAIL CODETHULU") ;
+		}
+	
+	
+	
+	
+	}
+	}
+	
+		
+	}
+	
+	
+	
